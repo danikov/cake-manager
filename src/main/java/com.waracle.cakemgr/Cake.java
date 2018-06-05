0b28c55c181489e2deb93629196617774a9bc3d2
Danikov <dandanikov@gmail.com>
2018-06-05 11:52:55 +0000

    ditch the spurious ID and just use title

--- src/main/java/com.waracle.cakemgr/Cake.java
+++ src/main/java/com.waracle.cakemgr/Cake.java
@@ -15,10 +15,6 @@ public class Cake implements Serializable {
     private static final long serialVersionUID = -1798070786993154676L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", unique = true, nullable = false)
-    private Integer cakeId;
-
     @Column(name = "TITLE", unique = true, nullable = false, length = 100)
     private String title;
 
