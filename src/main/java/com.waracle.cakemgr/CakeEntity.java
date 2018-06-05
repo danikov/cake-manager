35e294c5eb2b8c75a425243405077c1e03cb5dea
Danikov <dandanikov@gmail.com>
2018-06-05 11:13:15 +0000

    fix employee references to cake

--- src/main/java/com.waracle.cakemgr/CakeEntity.java
+++ src/main/java/com.waracle.cakemgr/CakeEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 
 @Entity
 @org.hibernate.annotations.Entity(dynamicUpdate = true)
-@Table(name = "Employee", uniqueConstraints = {@UniqueConstraint(columnNames = "ID"), @UniqueConstraint(columnNames = "EMAIL")})
+@Table(name = "Cake", uniqueConstraints = {@UniqueConstraint(columnNames = "ID"), @UniqueConstraint(columnNames = "TITLE")})
 @Data
 public class CakeEntity implements Serializable {
     private static final long serialVersionUID = -1798070786993154676L;
@@ -16,39 +16,14 @@ public class CakeEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", unique = true, nullable = false)
-    private Integer employeeId;
+    private Integer cakeId;
 
-    @Column(name = "EMAIL", unique = true, nullable = false, length = 100)
+    @Column(name = "TITLE", unique = true, nullable = false, length = 100)
     private String title;
 
-    @Column(name = "FIRST_NAME", unique = false, nullable = false, length = 100)
+    @Column(name = "DESCRIPTION", unique = false, nullable = false, length = 100)
     private String description;
 
-    @Column(name = "LAST_NAME", unique = false, nullable = false, length = 300)
+    @Column(name = "IMAGE", unique = false, nullable = false, length = 300)
     private String image;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
 }
