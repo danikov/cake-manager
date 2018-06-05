82f080f491c278976167f11c59548123ea8641fc
Danikov <dandanikov@gmail.com>
2018-06-05 11:53:10 +0000

    add repo accessors

--- src/main/java/com.waracle.cakemgr/CakeRepository.java
+++ src/main/java/com.waracle.cakemgr/CakeRepository.java
@@ -1,4 +1,20 @@
 package com.waracle.cakemgr;
 
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+
+import java.util.List;
+
+@AllArgsConstructor
 public class CakeRepository {
+    private final Session session;
+
+    public Cake loadBy(String title) {
+        return (Cake) session.get(Cake.class, title);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Cake> getAll() {
+        return (List<Cake>) session.createCriteria(Cake.class).list();
+    }
 }
