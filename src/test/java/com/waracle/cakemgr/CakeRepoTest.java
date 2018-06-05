7fd307e0461020187830880e24d43be8fad5377f
Danikov <dandanikov@gmail.com>
2018-06-05 11:10:04 +0000

    cake repo for HB stuff

---
+++ src/test/java/com/waracle/cakemgr/CakeRepoTest.java
@@ -0,0 +1,41 @@
+package com.waracle.cakemgr;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CakeRepoTest {
+    private SessionFactory sessionFactory;
+    private Session session = null;
+
+    @Before
+    public void before() {
+        sessionFactory =  HibernateUtil.getSessionFactory();
+        session = sessionFactory.openSession();
+    }
+
+    @Test
+    public void returnsCakeWithMatchingTitle() {
+        CakeEntity cake = new CakeEntity();
+        cake.setTitle("Unicorn Cake");
+        cake.setDescription("A rainbow sponge cake with glitter frosting");
+        cake.setImage("http://www.cupofsugarpinchofsalt.com/wp-content/uploads/2013/07/RainbowCake-small2.jpg");
+
+        session.save(cake);
+
+        CakeRepository cakeRepo = new CakeRepository(session);
+        CakeEntity loadedCake = cakeRepo.loadByTitle("Unicorn Cake");
+        assertNotNull(loadedCake);
+        assertEquals(loadedCake, cake);
+    }
+
+    @After
+    public void after() {
+        session.close();
+        sessionFactory.close();
+    }
+}
